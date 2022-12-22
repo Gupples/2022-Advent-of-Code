ab05adc2a01970f50057d3c029aba09afbebaddc
Jared Davey <dav15024@byui.edu>
2022-12-22 14:43:33 -0700

    Changed file names and comments

---
+++ Day 1/calorie-finder.py
@@ -0,0 +1,5 @@
+"""
+Each elf in elves.csv is represented by a space between a set of calories. Find the elf that has the largest
+calorie count and enter the total calories that elf has. 
+Strategy; Go through line by line. Run a total, and if the line is an empty space, reset the count. 
+"""
