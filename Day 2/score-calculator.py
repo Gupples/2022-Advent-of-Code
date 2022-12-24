f958487eb0091e3c74716568d42fd64fb29083ce
Jared Davey <dav15024@byui.edu>
2022-12-24 14:27:59 -0700

    Finished first draft of score-calculator.py

--- Day 2/score-calculator.py
+++ Day 2/score-calculator.py
@@ -8,17 +8,47 @@ Rock is A & X, 1 point
 Paper is B & Y, 2 points
 Scizzors is C & Z, 3 points
 """
+# Return points according to who won.
+def determineWinner(opponent, you):
+    win = 6
+    draw = 3
+    loss = 0
+    # Opponent wins
+    if ((opponent == "A" & you == "Z") or (opponent == "B" & you == "X") or
+        (opponent == "C" & you == "Y")):
+        return loss
+    # You win
+    elif ((you == "A" & opponent == "Z") or (you == "B" & opponent == "X") or
+        (you == "C" & opponent == "Y")):
+        return win
+    elif (opponent == you):
+        return draw
+
+
 def calculatePoints(filename):
+    tempPoints = []
+    
     with open(filename) as file:
         for line in file:
-            pass
+            opp = line[0]
+            you = line[1]
+            tempScore = 0
+            if(you == "X"):
+                tempScore = 1
+            elif(you == "Y"):
+                tempScore = 2
+            elif(you == "Z"):
+                tempScore = 3
+            tempScore += determineWinner(opp, you)
+            tempPoints.append(tempScore)
+    return tempPoints
 
 def display(score):
     print(f"Your point total is {score}\n")
 
 def main():
     points = []
-    calculatePoints("test-guide.txt")
+    points = calculatePoints("test-guide.txt")
     score = sum(points)
     display(score)
     
